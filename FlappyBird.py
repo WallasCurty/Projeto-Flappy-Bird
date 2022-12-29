f1b94e270755ca056b7d73e1f025eecf1a9c5a96
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2022-12-29 09:27:09 -0300

    Criação  Passaro
    Criação da classe pássaro e seus métodos

--- FlappyBird.py
+++ FlappyBird.py
@@ -19,7 +19,84 @@ FONTE_PONTOS = pygame.font.SysFont('arial', 50)
 
 
 class Passaro:
-    pass
+    
+    IMGS = IMAGENS_PASSARO
+    #ANIMAÇÕES DA ROTAÇÃO
+    ROTACAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contegem_imagem = 0
+        self.imagem = self.IMGS[0]
+
+    def pular(self):
+        self.velocidade = 10,5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+
+        #Calcular o deslocamento
+        self.tempo +=1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+
+        #Restringir o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+        #Angulo do passaro
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
+        else:
+            if self.angulo > -90:
+               self.angulo -= self.VELOCIDADE_ROTACAO
+
+    def desenhar(self, tela):
+        # definir a imagem a ser usada
+        self.contegem_imagem += 1
+
+        if self.contegem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        
+        elif self.contegem_imagem < self.TEMPO_ANIMACAO*2:
+            self.imagem = self.IMGS[1]
+
+        elif self.contegem_imagem < self.TEMPO_ANIMACAO*3:
+            self.imagem = self.IMGS[2]
+
+        elif self.contegem_imagem < self.TEMPO_ANIMACAO*4:
+            self.imagem = self.IMGS[1]
+
+        elif self.contegem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
+            self.imagem = self.IMGS[0]
+            self.contegem_imagem = 0  
+
+        # se o passaro cair, não vai bater a asa
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contegem_imagem = self.TEMPO_ANIMACAO*2
+
+        # desenhar imagem
+        imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+        pos_centro_imagem = self.imagem.get_rect(topleft = (self.x , self.y)).center
+        retangulo = imagem_rotacionada.get_rect(center = pos_centro_imagem)
+        tela.blid(imagem_rotacionada, retangulo.topleft)
+        
+
+    def get_mask(self):
+        pygame.mask.from_surface(self.imagem)
 
 class Cano:
     pass
